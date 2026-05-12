455948e79cc14f927db3d6fbd6bb5f70900d23aa
lizaferns <liza.fernandes2425@xaviers.edu.in>
2026-05-12 17:08:08 +0530

    Update list_users.py

--- scratch/list_users.py
+++ scratch/list_users.py
@@ -3,7 +3,7 @@ import mysql.connector
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "WILL@_1981",
+    "password": "VaLIaMpwaJescUNEcyBlCuInrFbovJPI",
     "database": "cet_predictor"
 }
 
